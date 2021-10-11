5a983c151323bd7470dd1ebe1bae7363444c2db3
Skicom123 <fajarrahmanarajab@gmail.com>
2021-10-11 23:00:57 +0700

    Problem4

---
+++ Assets/Scripts/BallMovementConstant.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallMovementConstant : MonoBehaviour
+{
+    private Rigidbody2D rb;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    private void FixedUpdate()
+    {
+        rb.velocity = new Vector2 (5f,0f);
+    }
+}
---
+++ Assets/Scripts/BallProblem4.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallProblem4 : MonoBehaviour
+{
+    public float moveSpeed = 5f;
+    public Rigidbody2D rb;
+    Vector2 movement;
+
+    void Update()
+    {
+        movement.x = Input.GetAxisRaw("Horizontal") *moveSpeed;
+        movement.y = Input.GetAxisRaw("Vertical") * moveSpeed;
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        rb.velocity = new Vector2(movement.x,movement.y);
+    }
+}
