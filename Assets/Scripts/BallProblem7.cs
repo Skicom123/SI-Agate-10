4d030a4c627a8db47304e166d3fbc3fb587e0f6f
Skicom123 <fajarrahmanarajab@gmail.com>
2021-10-11 23:05:38 +0700

    Problem6
    menambahkan  random box muncul

---
+++ Assets/Scripts/BallProblem7.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class BallProblem7 : MonoBehaviour
+{
+    public Text scoreText;
+    private int score;
+
+    private void Start()
+    {
+        score = 0;
+        scoreText.text = "Score : " + score;
+    }
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+
+        if(collision.tag == "Box")
+        {
+            score++;
+            Destroy(collision.gameObject);
+            scoreText.text = "Score : " + score;
+        }
+    }
+}
