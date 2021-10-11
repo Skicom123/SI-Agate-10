5a3ccfd9b7ca7b4ca9aee6c142d8f9648bf6ebed
Skicom123 <fajarrahmanarajab@gmail.com>
2021-10-11 22:55:39 +0700

    problem2

---
+++ Assets/Scripts/BallProblem5.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallProblem5 : MonoBehaviour
+{
+    Vector2 clickArea;
+    public float speed = 12f;
+
+    bool moving;
+
+    private void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            clickArea = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            moving = true;
+        }
+
+        if(moving && (Vector2)transform.position != clickArea)
+        {
+            float step = speed * Time.deltaTime;
+            transform.position = Vector2.MoveTowards(transform.position, clickArea, step);
+        }
+        else
+        {
+            moving = false;
+        }
+    }
+}
---
+++ Assets/Scripts/BoxSpawner.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BoxSpawner : MonoBehaviour
+{
+    public GameObject box;
+    public GameObject triangle;
+    private void Start()
+    {
+        Invoke("Spawn", 1f);
+        Invoke("SpawnTri",1f);
+    }
+
+    public void Spawn()
+    {
+        float posX, posY;
+        Vector2 pos;
+
+        int numberToSpawn = Random.Range(4, 5);
+
+        for(int i = 0; i< numberToSpawn; i++)
+        {
+            posX = Random.Range(-3, 3);
+            posY = Random.Range(-3, 3);
+            pos = new Vector2(posX, posY);
+
+            Instantiate(box, pos, box.transform.rotation);
+        }
+    }
+    public void SpawnTri()
+    {
+        float posX, posY;
+        Vector2 pos;
+
+        int numberToSpawn = Random.Range(1, 3);
+
+        for(int i = 0; i< numberToSpawn; i++)
+        {
+            posX = Random.Range(-3, 3);
+            posY = Random.Range(-3, 3);
+            pos = new Vector2(posX, posY);
+
+            Instantiate(triangle, pos, triangle.transform.rotation);
+        }
+    }
+}
+
